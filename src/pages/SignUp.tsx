d1d1a8cfb5f78eee39ab5ccb54fa9ae690f24c14
JosephAwuku33 <33jaykay@gmail.com>
2023-06-19 09:08:29 +0000

    tailwind color config

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React, {useState} from "react";
+
+//Will fill this in soon
+function SignUp() {
+    return (
+      <View>
+
+      </View>
+    );
+}
+
+export default SignUp;
