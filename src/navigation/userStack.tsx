d8fe1222f68b3af94b2ff61c24dde210325f68b9
JosephAwuku33 <33jaykay@gmail.com>
2023-06-21 11:53:26 +0000

    fixed navigation issues

--- src/navigation/userStack.tsx
+++ src/navigation/userStack.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from '../pages/Home';
 
@@ -7,10 +6,8 @@ const Stack = createStackNavigator();
 
 export default function UserStack() {
   return (
-    <NavigationContainer>
         <Stack.Navigator>
             <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
         </Stack.Navigator>
-    </NavigationContainer>
   )
 }
