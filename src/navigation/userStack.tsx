93497869afa47fd981ac96c5e2a5d37bb6d27319
JosephAwuku33 <33jaykay@gmail.com>
2023-06-26 16:44:04 +0000

    Added Custom Icon

--- src/navigation/userStack.tsx
+++ src/navigation/userStack.tsx
@@ -3,13 +3,18 @@ import HomeScreen from '../pages/Home';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Inventory from "../pages/Inventory";
 import Transactions from "../pages/Transactions";
+import CustomIcon from '../components/icons/Icon';
 
 const Drawer = createDrawerNavigator();
 
 export default function UserStack() {
   return (
     <Drawer.Navigator initialRouteName="Home" >
-        <Drawer.Screen name="Home" component={HomeScreen}/>
+        <Drawer.Screen name="Home" component={HomeScreen} options={{drawerIcon: ({color, size}) => (
+            <CustomIcon
+               name="md-home" size={size} color={color = "#552619"}
+            />
+          )}}/>
         <Drawer.Screen name="Inventory" component={Inventory} />
         <Drawer.Screen name="Transactions" component={Transactions} />
     </Drawer.Navigator>
