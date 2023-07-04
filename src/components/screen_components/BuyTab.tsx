a1aea147169b70820806d7955770516785a45557
JosephAwuku33 <33jaykay@gmail.com>
2023-07-04 13:03:44 +0000

    working on the BuyTab component

--- src/components/screen_components/BuyTab.tsx
+++ src/components/screen_components/BuyTab.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View, TextInput } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function BuyTab() {
   return (
-    <View className="bg-secondary flex h-screen items-center mt-5">
-        <Text style={{ fontFamily: "Poppins-Regular" }}>Buy Tab</Text>
+    <View className="bg-secondary h-screen">
+      <View className="flex items-center mt-3">
+        <Text style={{ fontFamily: "Poppins-Regular" }}>Buy Items</Text>
+      </View>
+      <KeyboardAwareScrollView>
+        <View className="flex flex-col justify-center p-6 gap-3 mb-2">
+          <View></View>
+          <TextInput
+            placeholder="Customer Name"
+            className="bg-white rounded-full border-2 border-primary p-1 text-center"
+          />
+        </View>
+      </KeyboardAwareScrollView>
     </View>
-  )
+  );
 }
