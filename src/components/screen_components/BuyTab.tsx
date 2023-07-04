261396024098b9088d084bb4f5aabd22ab3900d3
JosephAwuku33 <33jaykay@gmail.com>
2023-07-04 12:52:30 +0000

    working on BuyTab

--- src/components/screen_components/BuyTab.tsx
+++ src/components/screen_components/BuyTab.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 export default function BuyTab() {
   return (
-    <View className="bg-secondary flex h-screen items-center justify-center">
+    <View className="bg-secondary flex h-screen items-center mt-5">
         <Text style={{ fontFamily: "Poppins-Regular" }}>Buy Tab</Text>
     </View>
   )
