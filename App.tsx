7e2eb49967ceb892d3031393b84a4b005c3c9190
JosephAwuku33 <33jaykay@gmail.com>
2023-07-10 16:17:54 +0000

    adding products tab

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import RootNavigation from './src/navigation';
 import './config/firebase';
+import { LogBox } from 'react-native';
 
 //import { useEffect } from "react"
 //import * as Font from "expo-font";
@@ -12,7 +13,7 @@ export default function App() {
    let [fontsLoaded] = useFonts({
       'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf')
     });
-  
+    LogBox.ignoreLogs(['Invalid prop textStyle of type array supplied to Cell']);
     if (!fontsLoaded) {
       return null;
     }
--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import AllTab from "../components/screen_components/AllTab";
 import NearExpiryTab from "../components/screen_components/NearExpiryTab";
+import AddProductTab from "../components/screen_components/AddProductTab";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -18,6 +19,7 @@ export default function Inventory() {
       }}>
           <Tab.Screen name="AllTab" component={AllTab} options={{tabBarLabel: "All"}}  />
           <Tab.Screen name="NearExpiryTab" component={NearExpiryTab} options={{tabBarLabel: "Near Expiry"}}/>
+          <Tab.Screen name="AddProductTab" component={AddProductTab} options={{tabBarLabel: "Add"}}/>
       </Tab.Navigator>
   );
 }
