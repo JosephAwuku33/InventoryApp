aabf8015e22f087fcebb55d01b4a5cae53a53603
JosephAwuku33 <33jaykay@gmail.com>
2023-07-10 01:07:26 +0000

    added in tables

--- src/components/screen_components/AllTab.tsx
+++ src/components/screen_components/AllTab.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import { View, Text } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Table, Row, Rows } from "react-native-table-component";
+import { useInventoryContext } from "../../context/InventoryContext";
 
 
 export default function AllTab() {
+  const tableHead = ["Product", "Price"];
+
+  const inventoryContext = useInventoryContext();
+  const { inventory } = inventoryContext;
+  const tableData = inventory.map(item => [item.name, item.price.toString()]);
+  
+
   return (
-    <View className="bg-secondary flex h-screen items-center justify-center">
-        <Text style={{ fontFamily: "Poppins-Regular" }}>All Tabs</Text>
-    </View>
-  )
+    <KeyboardAwareScrollView>
+      <View style={styles.container}>
+        <Table borderStyle={styles.table}>
+          <Row data={tableHead} textStyle={styles.text} style={styles.head}  />
+          <Rows data={tableData} textStyle={styles.text}  />
+        </Table>
+      </View>
+    </KeyboardAwareScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#FBEAE7' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 },
+  table: { borderWidth: 1, borderColor: '#000' },
+});
