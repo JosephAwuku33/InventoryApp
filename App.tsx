f5a2a6ac996c0ab6f176e500717e2831853b0bb4
JosephAwuku33 <33jaykay@gmail.com>
2023-07-11 04:56:39 +0000

    fixed authentication db linkage

--- App.tsx
+++ App.tsx
@@ -1,11 +1,8 @@
 import 'react-native-gesture-handler';
 import RootNavigation from './src/navigation';
 import './config/firebase';
-import { LogBox } from 'react-native';
-
-//import { useEffect } from "react"
-//import * as Font from "expo-font";
 import { useFonts } from "expo-font";
+
 
 
  
@@ -13,7 +10,7 @@ export default function App() {
    let [fontsLoaded] = useFonts({
       'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf')
     });
-    LogBox.ignoreLogs(['Invalid prop textStyle of type array supplied to Cell']);
+    
     if (!fontsLoaded) {
       return null;
     }
