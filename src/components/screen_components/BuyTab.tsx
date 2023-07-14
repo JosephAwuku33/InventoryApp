6c25b8faadc5725663d06b028f59cd69d6891efa
JosephAwuku33 <33jaykay@gmail.com>
2023-07-14 13:28:40 +0000

    added Purchasing option

--- src/components/screen_components/BuyTab.tsx
+++ src/components/screen_components/BuyTab.tsx
@@ -2,23 +2,70 @@ import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { SelectList } from "react-native-dropdown-select-list";
 import Spinner from "react-native-loading-spinner-overlay";
+import Toast from "react-native-root-toast";
 import { useInventoryContext } from "../../context/InventoryContext";
 import useSelectedItem from "../../hooks/useSelectedItem";
+import { useState } from "react";
+import { db, auth } from "../../../config/firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 export default function BuyTab() {
+  const [customerName, setCustomerName] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [validationMessage, setValidationMessage] = useState<string>("");
+  const [loading, setLoading] = useState(false);
 
+  const userId = auth.currentUser?.uid;
   const inventoryContext = useInventoryContext();
-  const { isLoading: inventoryLoading, inventory  } = inventoryContext;
-  const { quantity, expiryDate, updateSelectedItem} = useSelectedItem(inventory);
+  const { inventory } = inventoryContext;
+  const { quantity, expiryDate, updateSelectedItem, selected } =
+    useSelectedItem(inventory);
+  const purchaseCollectionRef = collection(db, "Purchase");
   const inventorySelectList = [...inventory.map((inventory) => inventory.name)];
 
-  {console.log(expiryDate)}
+  const addPurchaseInventory = async () => {
+    if (
+      customerName == " " ||
+      phoneNumber == " " ||
+      address == " " ||
+      selected == " "
+    ) {
+      setValidationMessage("Required field missing");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await addDoc(purchaseCollectionRef, {
+        id: userId,
+        customerName,
+        customerNumber: phoneNumber,
+        price: quantity[0],
+        productName: selected,
+        purchaseDate: new Date(),
+        status: "Buy",
+      });
+      Toast.show("Inventory item added successfully", {
+        duration: Toast.durations.SHORT,
+      });
+    } catch (error) {
+      console.log("Error adding item", error);
+      Toast.show("Error adding item", {
+        duration: Toast.durations.LONG,
+      });
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <KeyboardAwareScrollView>
       <View className="bg-secondary h-screen">
         <Spinner
-          visible={inventoryLoading}
-          textContent={"Loading..."}
+          visible={loading}
+          textContent={"Completing Transaction..."}
           textStyle={{ color: "#FFF" }}
         />
         <View className="flex items-center mt-3">
@@ -29,16 +76,20 @@ export default function BuyTab() {
           <TextInput
             placeholder="Customer Name"
             className="bg-white rounded-full border-2 border-primary p-1 text-center"
+            onChangeText={(text) => setCustomerName(text)}
           />
           <TextInput
             placeholder="Phone Number"
             className="bg-white rounded-full border-2 border-primary p-1 text-center"
+            onChangeText={(text) => setPhoneNumber(text)}
           />
           <TextInput
             placeholder="Address"
             className="bg-white rounded-full border-2 border-primary p-1 text-center"
+            onChangeText={(text) => setAddress(text)}
           />
           <View className=" flex items-center border-primary border-2 mb-4" />
+
           <View>
             <SelectList
               data={inventorySelectList}
@@ -64,17 +115,12 @@ export default function BuyTab() {
             value={expiryDate.toString()}
             className="bg-white rounded-full border-2 border-primary text-black p-1 text-center"
           />
-         
+          <Text className="mt-2 text-black text-sm">{validationMessage}</Text>
           <View className="flex items-center justify-center">
-            <TouchableOpacity>
-              <Text
-                style={{ fontFamily: "Poppins-Regular" }}
-                className="text-center text-primary text-sm"
-              >
-                + Add Product
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity className="bg-primary mt-2 px-20 py-2 rounded-full">
+            <TouchableOpacity
+              className="bg-primary mt-2 px-20 py-2 rounded-full"
+              onPress={addPurchaseInventory}
+            >
               <Text className="text-center text-white text-xl font-semibold ">
                 Buy
               </Text>
